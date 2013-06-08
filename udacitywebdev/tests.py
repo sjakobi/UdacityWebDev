7d8f4667bddc8e95eee2dd78caa5b871d4aad126
sjakobi <simon.jakobi@gmail.com>
2013-06-08 06:11:28 +0200

    Added tests.py. Tests run ok.

---
+++ udacitywebdev/tests.py
@@ -0,0 +1,30 @@
+import unittest
+
+from pyramid import testing
+
+
+class ViewTests(unittest.TestCase):
+    def setUp(self):
+        self.config = testing.setUp()
+
+    def tearDown(self):
+        testing.tearDown()
+
+    def test_my_view(self):
+        from udacitywebdev.views import hello_udacity
+        request = testing.DummyRequest()
+        response = hello_udacity(request)
+        self.assertEqual(response.status, "200 OK")
+
+
+class FunctionalTests(unittest.TestCase):
+    def setUp(self):
+        from udacitywebdev import main
+        settings = {}
+        app = main(settings)
+        from webtest import TestApp
+        self.testapp = TestApp(app)
+
+    def test_it(self):
+        res = self.testapp.get("/", status=200)
+        self.assertIn(b"Hello, Udacity!", res.body)
