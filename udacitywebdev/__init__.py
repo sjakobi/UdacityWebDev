d8346460683afcbb360c6f29c93a7bb49883747d
sjakobi <simon.jakobi@gmail.com>
2013-06-08 06:03:38 +0200

    1 bug fixed. app.py no longer necessary

--- udacitywebdev/__init__.py
+++ udacitywebdev/__init__.py
@@ -2,6 +2,6 @@ from pyramid.config import Configurator
 
 def main(global_config, **settings):
     config = Configurator(settings=settings)
-    config.add_route("hello", "/")
+    config.add_route("front", "/")
     config.scan()
     return config.make_wsgi_app()
